4870e284b8bde608d40b7f25a5f6d0be08f66d7a
mrahal <rahalmichel@hotmail.fr>
2023-01-11 21:05:39 +0100

    Initial commit

---
+++ app/src/main/java/com/example/projetbook/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.projetbook
+
+import android.media.Image
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
