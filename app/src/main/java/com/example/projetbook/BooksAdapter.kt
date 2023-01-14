a05991e2c19eda5b2ebe36d928969ba459067e75
mrahal <rahalmichel@hotmail.fr>
2023-01-14 11:24:24 +0100

    version stable with load more data

--- app/src/main/java/com/example/projetbook/BooksAdapter.kt
+++ app/src/main/java/com/example/projetbook/BooksAdapter.kt
@@ -1,36 +1,69 @@
 package com.example.projetbook
 
-import android.content.Intent
+import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import android.graphics.drawable.Drawable
-import android.net.Uri
 import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.squareup.picasso.Picasso
-import java.io.InputStream
-import java.net.URL
 import java.util.concurrent.Executors
-import java.util.logging.Handler
 
 
-class BooksAdapter(private val dataset: Array<BookType>, val listener: (BookType) -> Unit) :
+class BooksAdapter(private val dataset: Array<BookType>, val listener: (BookType) -> Unit, val context: Context) :
     RecyclerView.Adapter<BooksAdapter.ViewHolder>() {
+
+
+
     class ViewHolder(view : View) : RecyclerView.ViewHolder(view) {
         fun bind(book: BookType, listener: (BookType) -> Unit) = with(itemView)
         {
             itemView.findViewById<TextView>(R.id.title).text = book.title;
             //itemView.findViewById<TextView>(R.id.description).text = book.description
             if (book.illustration.isEmpty()) {
-                itemView.findViewById<ImageView>(R.id.illustration).setImageResource(R.mipmap.book_sample)
+                itemView.findViewById<ImageView>(R.id.illustration).setImageResource(R.mipmap.book_sample_foreground)
             } else {
-                Picasso.get().load(book.illustration).into(itemView.findViewById<ImageView>(R.id.illustration))
+                // Declaring and initializing the ImageView
+                val imageView = findViewById<ImageView>(R.id.illustration)
+
+                // Declaring executor to parse the URL
+                val executor = Executors.newSingleThreadExecutor()
+
+                // Once the executor parses the URL
+                // and receives the image, handler will load it
+                // in the ImageView
+                val handler = android.os.Handler(Looper.getMainLooper())
+
+                // Initializing the image
+                var image: Bitmap? = null
+
+                // Only for Background process (can take time depending on the Internet speed)
+                executor.execute {
+
+                    // Image URL
+                    val imageURL = book.illustration
+
+                    // Tries to get the image and post it in the ImageView
+                    // with the help of Handler
+                    try {
+                        val `in` = java.net.URL(imageURL).openStream()
+                        image = BitmapFactory.decodeStream(`in`)
+
+                        // Only for making changes in UI
+                        handler.post {
+                            imageView.setImageBitmap(image)
+                        }
+                    }
+
+                    // If the URL doesnot point to
+                    // image or any other kind of failure
+                    catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+                }
             }
             /*itemView.findViewById<Button>(R.id.linkToBook).setOnClickListener {
                 val browserIntent =
@@ -43,13 +76,14 @@ class BooksAdapter(private val dataset: Array<BookType>, val listener: (BookType
     }
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(viewGroup.context)
+        val view = LayoutInflater.from(context)
             .inflate(R.layout.line_book, viewGroup, false)
         return ViewHolder(view);
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(dataset[position], listener);
+        val book : BookType = dataset[position]
+        holder.bind(book, listener);
     }
 
     override fun getItemCount(): Int {
--- app/src/main/java/com/example/projetbook/SearchActivity.kt
+++ app/src/main/java/com/example/projetbook/SearchActivity.kt
@@ -1,63 +1,156 @@
 package com.example.projetbook
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.NestedScrollView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerView.LayoutManager
-import org.json.JSONObject
-import org.json.JSONTokener
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.VolleyError
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import com.loopj.android.http.AsyncHttpClient
+import com.loopj.android.http.JsonHttpResponseHandler
+import cz.msebera.android.httpclient.Header
+
 
 class SearchActivity : AppCompatActivity() {
-    lateinit var monRecycler: RecyclerView
+    private var recyclerView: RecyclerView? = null
+    lateinit var books: ArrayList<BookType>
+    private var bookAdapter: BooksAdapter? = null
+
+
+    private var nestedSV: NestedScrollView? = null
+
+    private var loadingPB: ProgressBar? = null
+
+    // creating a variable for our page and limit as 2
+    // as our api is having highest limit as 2 so
+    // we are setting a limit = 2
+    private var page: Int = 0
+    private var limit: Int = 10
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.results_search)
         //bouchon
-        val bundle : Bundle? = intent.extras
-        val json: String? = intent.getStringExtra("results")
+        nestedSV = findViewById(R.id.idNestedSV)
 
-        val books = getResults(json)
+        loadingPB = findViewById(R.id.idPBLoading);
+        books = ArrayList()
 
-        val booksAdapter : BooksAdapter = BooksAdapter(books, { book -> bookOnclick() })
+        //val booksAdapter : BooksAdapter = BooksAdapter(books, { book -> bookOnclick() })
 
-        val recyclerView : RecyclerView = findViewById(R.id.recycler_view)
-        recyclerView.layoutManager  = LinearLayoutManager(this)
-        recyclerView.adapter = booksAdapter
+        recyclerView = findViewById(R.id.recycler_view)
+        recyclerView!!.layoutManager  = LinearLayoutManager(this@SearchActivity)
+        recyclerView!!.adapter = BooksAdapter(books.toTypedArray(), {book -> bookOnclick()}, this@SearchActivity)
 
+        getResults(page, limit)
+
+        // adding on scroll change listener method for our nested scroll view.
+        // adding on scroll change listener method for our nested scroll view.
+        nestedSV?.setOnScrollChangeListener(NestedScrollView.OnScrollChangeListener { v, scrollX, scrollY, oldScrollX, oldScrollY ->
+            // on scroll change we are checking when users scroll as bottom.
+            if (scrollY == v.getChildAt(0).measuredHeight - v.measuredHeight) {
+                // in this method we are incrementing page number,
+                // making progress bar visible and calling get data method.
+                page++
+                loadingPB?.visibility = View.VISIBLE
+                getResults(page, limit)
+            }
+        })
     }
 
     private fun bookOnclick() {
         TODO("Not yet implemented")
     }
 
-    fun getResults(results : String?) : Array<BookType> {
-        val jsonObject = JSONTokener(results).nextValue() as JSONObject
+    fun getResults(page : Int, limit : Int) {
 
-        val jsonArray = jsonObject.getJSONArray("items")
+        if (page > limit) {
+            Log.i("FOUND : ", "ON Y EST")
+            // checking if the page number is greater than limit.
+            // displaying toast message in this case when page>limit.
+            Toast.makeText(this, "C'est tout pour le moment...", Toast.LENGTH_SHORT).show();
 
-        val books = ArrayList<BookType>()
-
-        for (i in 0 until jsonArray.length()) {
-            val imagelinks =
-                jsonArray.getJSONObject(i).getJSONObject("volumeInfo").optJSONObject("imageLinks")
-            val image = if (imagelinks == null) "" else imagelinks.getString("smallThumbnail");
-            /*val description =
-                jsonArray.getJSONObject(i).getJSONObject("volumeInfo").optString("description")
-                    ?: ""*/
-
-            books.add(
-                BookType(
-                    i.toLong(),
-                    jsonArray.getJSONObject(i).getJSONObject("volumeInfo").getString("title"),
-                    image
-                    //jsonArray.getJSONObject(i).getJSONObject("volumeInfo").getString("previewLink")
-                )
-            )
+            // hiding our progress bar.
+            loadingPB?.visibility = View.GONE;
+            return;
         }
 
+        // creating a new variable for our request queue
+        val queue = Volley.newRequestQueue(this@SearchActivity)
 
-        return books.toTypedArray()
+        val bundle : Bundle? = intent.extras
+        val text: String? = intent.getStringExtra("key")
+
+        //val client : AsyncHttpClient = AsyncHttpClient();
+        val jsonRequest = JsonObjectRequest(Request.Method.GET,"https://www.googleapis.com/books/v1/volumes?q=$text&startIndex=$page", null,
+            { jsonObject ->
+                try {
+                    Log.i("FOUND : ", jsonObject.toString())
+
+                    val jsonArray = jsonObject.getJSONArray("items")
+
+                    for (i in 0 until jsonArray.length()) {
+                        val imagelinks =
+                            jsonArray.getJSONObject(i).getJSONObject("volumeInfo").optJSONObject("imageLinks")
+                        val image = if (imagelinks == null) "" else imagelinks.getString("smallThumbnail");
+                        /*val description =
+                                        jsonArray.getJSONObject(i).getJSONObject("volumeInfo").optString("description")
+                                            ?: ""*/
+
+                        books.add(
+                            BookType(
+                                i.toLong(),
+                                jsonArray.getJSONObject(i).getJSONObject("volumeInfo").getString("title"),
+                                image
+                                //jsonArray.getJSONObject(i).getJSONObject("volumeInfo").getString("previewLink")
+                            )
+                        )
+                    }
+
+
+                    bookAdapter = BooksAdapter(books.toTypedArray(), { book -> bookOnclick() }, this@SearchActivity)
+                    recyclerView!!.layoutManager  = LinearLayoutManager(this@SearchActivity)
+                    recyclerView!!.adapter = bookAdapter
+                    bookAdapter!!.notifyDataSetChanged()
+                    loadingPB?.visibility = View.INVISIBLE
+
+
+                } catch (e : Exception) {
+                    e.printStackTrace()
+                }
+            },
+            { error ->
+                        Log.e("TAG", "RESPONSE IS $error")
+                        // handling on error listener method.
+                        Toast.makeText(
+                            this@SearchActivity,
+                            "Impossible de récupérer les résultats..",
+                            Toast.LENGTH_SHORT
+                        ).show();
+            }
+        )
+        /*val request = client.get("https://www.googleapis.com/books/v1/volumes?q=$text",
+            object : JsonHttpResponseHandler() {
+            override fun onSuccess(
+                statusCode: Int,
+                headers: Array<out Header>?,
+                response: JSONObject?
+            ) {
+
+
+            }
+
+        })*/
+
+
+        queue.add(jsonRequest);
     }
 }
